c2adb163626000c882cb92614d27c6b719650efe
jiko21 <daikikojima0531@icloud.com>
2022-09-25 00:10:53 +0900

    add: page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import type { AppProps } from 'next/app'
+import 'styles/styles.css';
+import { defaultTheme, Text, Provider, SSRProvider } from "@adobe/react-spectrum";
+import { Header } from "@/components/organisms/Header";
+import { DefaultTemplate } from "@/components/templates/DefaultTemplate";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({Component, pageProps}: AppProps) => (
+  <SSRProvider>
+    <Provider theme={defaultTheme}>
+      <DefaultTemplate
+        header={
+          <Header>
+            <Text height="size-200" margin="size-1000">
+              Colory
+            </Text>
+          </Header>
+        }
+      >
+        <Component {...pageProps} />
+      </DefaultTemplate>
+    </Provider>
+  </SSRProvider>
+);
 
 export default MyApp
